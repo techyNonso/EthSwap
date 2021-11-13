944dad41b0e8b4ed81838f7268305b5d85519c11
william ikeji <williamikeji@gmail.com>
2021-11-13 22:50:45 +0100

    token deplyed and EthSawp has all tokens

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
+
+module.exports = async function(deployer) {
+    //deploy Token
+    await deployer.deploy(Token);
+    const token = await Token.deployed()
+    //deploy EthSwap
+    await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed()
+
+    //transfer all tokens to eth swap
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+    
+};
