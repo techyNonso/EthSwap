7367a80d0135fe5f49d171c91e757ec6a9625404
william ikeji <williamikeji@gmail.com>
2021-11-16 15:24:59 +0100

    deployed to ropsten

---
+++ build/precache-manifest.75fb0ace27ce625238df0e6d2e361b65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae83741c0da8bd1674fe",
+    "url": "/static/css/main.770c12a5.chunk.css"
+  },
+  {
+    "revision": "ae83741c0da8bd1674fe",
+    "url": "/static/js/main.ae83741c.chunk.js"
+  },
+  {
+    "revision": "d827cb29bc320988d7a4",
+    "url": "/static/css/1.4797de9b.chunk.css"
+  },
+  {
+    "revision": "d827cb29bc320988d7a4",
+    "url": "/static/js/1.d827cb29.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c1d9e5f77d100c5ee8fb7c5e9057704",
+    "url": "/static/media/token-logo.6c1d9e5f.png"
+  },
+  {
+    "revision": "d2d0aa54b50a55c904fa077126f7dedb",
+    "url": "/index.html"
+  }
+];
