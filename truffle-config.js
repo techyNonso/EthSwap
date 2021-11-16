785ea4d857ac9f0219e081127f21d8b1e19f1e4f
william ikeji <williamikeji@gmail.com>
2021-11-16 15:22:04 +0100

    deployed to ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,28 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const infuraKey = "d0dc45555a2743d19db5b23c8a5babb0"
+
+const fs =  require("fs")
+const mnemonic = fs.readFileSync(".secret").toString().trim()
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      development: {
+        host: "127.0.0.1",
+        port: 7545,
+        network_id: "*" // Match any network id
+      },
+      ropsten:{
+        provider: new HDWalletProvider(
+        mnemonic,
+        `https://ropsten.infura.io/v3/${infuraKey}`
+      ),
+      network_id: 3,
+      gas: 5500000,
+      confirmations:2,
+      timeoutBlocks:200,
+      skipDryRun:true
     },
   },
   contracts_directory: './src/contracts/',
